63ac7ed72091cba10dea4281ef7e96b909d41de3
16Byte <pixeldevs.diego@gmail.com>
2015-07-14 11:32:32 -0600

    These are the source files to my Native Mod Installer

---
+++ Assets/InstallButtons.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class InstallButtons : MonoBehaviour {
+
+    public Text installButton;
+
+    void Start()
+    {
+        installButton.text = "Install Mod";
+    }
+
+}
